90d6ce392918cb7e4c41ee80f7bf675d733ec548
Matt Schroeder <matthew.schroeder@improving.com>
2022-10-21 14:01:48 -0500

    fix entity

--- postgres/src/main/java/org/msse/demo/customer/profile/CustomerEntity.java
+++ postgres/src/main/java/org/msse/demo/customer/profile/CustomerEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "com.msse.demo.customer")
+@Table(name = "customer")
 public class CustomerEntity {
     @Id
     private String id;
